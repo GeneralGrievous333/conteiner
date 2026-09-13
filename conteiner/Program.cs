8a36ed4224c21778dcc83fd6c4e274fa26ceabdd
TestNameFork <TestEmailFork>
2026-09-13 13:58:03 +0300

    Добавьте файлы проекта.

---
+++ conteiner/Program.cs
@@ -0,0 +1,10 @@
+﻿public struct PointStruct { public int X; public int Y;  }
+class PointClass { public int X; public int Y;}
+class Programm
+{
+    static void Main(string[] args)
+    {
+        PointStruct a1 = new PointStruct() { X = 1, Y = 2};
+        PointClass a2 = new PointClass() { X = 3, Y = 4};
+    }
+}
